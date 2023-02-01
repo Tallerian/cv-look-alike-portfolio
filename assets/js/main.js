f2513db0449f9298a9d1f288b25b5ec6cf9813f2
Ian Ruvuto <tallerian5@gmail.com>
2023-02-01 13:39:40 +0100

    Social Mediaas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,34 @@
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml10 .myprofile-text');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+var hello = document.getElementById('myprofile-text')
+const phrases = ['Hello my name is Ian Ruvuto Kampwanyi', 'Welcome to my CV-Look-Alike Portfolio ']
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml10 .myprofile-text',
-    rotateY: [-90, 0],
-    duration: 2300,
-    delay: (el, i) => 45 * i
-  })
-  .add({
-    targets: '.ml10',
-    opacity: 0,
-    duration: 2000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+var i = 0
+var j = 0
+
+current_txt = []
+
+function loop_my_profile_txt(){
+
+  hello.innerHTML = current_txt.join('')
+
+  if (i < phrases.length){
+    if(j < phrases[i].length){
+      current_txt.push(phrases[i][j])
+      j++
+    }
+  }
+
+  if( j == phrases[i].length){
+    i++
+    j = 0
+    current_txt.push("<br>")
+  }
+  setTimeout(loop_my_profile_txt, 40)
+
+}
+
+loop_my_profile_txt()
+
+
+  
+

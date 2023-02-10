c8e50cb52eef35d7ffdad455f3036cffee5205c7
Ian Ruvuto <tallerian5@gmail.com>
2023-02-10 20:08:34 +0100

    Social Mediass added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,17 @@
-// Wrap every letter in a span
+let api_url = null
+
+//Wrap every letter in a span
 var hello = document.getElementById('myprofile-text')
+var project_text1 = document.getElementById('proj_text')
 const phrases = ['Hello my name is Ian Ruvuto Kampwanyi', 'Welcome to my CV-Look-Alike Portfolio ']
+const proj_text1 = "This is a React Native app project This is a React Native app project This is a React Native app project"
 
 var i = 0
 var j = 0
+
+var next = null
+
+var done = false
 
 current_txt = []
 
@@ -17,6 +25,11 @@ function loop_my_profile_txt(){
       j++
     }
   }
+  else{
+    done = true
+    console.log(done)
+    return
+  }
 
   if( j == phrases[i].length){
     i++
@@ -27,7 +40,33 @@ function loop_my_profile_txt(){
 
 }
 
-loop_my_profile_txt()
+
+function loop_project_text(){
+  project_text1.innerHTML = current_txt.join('')
+  
+  if (count < proj_text1.length){
+    current_txt.push(proj_text1[count])
+  }
+  else{
+    return
+  }
+  count++
+  setTimeout(loop_project_text, 40)
+}
+
+function translate_eng(text_edit, lang){
+  api_url = `https://api.mymemory.translated.net/get?q=${text_edit}&langpair=${lang}|it`;
+  fetch(api_url).then(res => res.json().then(data => {
+    console.log(data)
+  }));
+}
+
+count = 0;
+loop_project_text();
+
+
+
+
 
 
   
